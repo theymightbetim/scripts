7e78bc0b8474f0396a45188b65dd95ad0f8b5261
Tim Mackenzie <theymightbetim@gmail.com>
2025-04-25 19:23:18 -0400

    installation script

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
-SCRIPTS=`ls -I "*.MD" -I "*.sh" | tr " " "\n"`
-echo $SCRIPTS
-echo $SCRIPTS | xargs -I {} cp {} ~/.local/bin/
- 
+SCRIPT=`ls -I "*.MD" -I "*.sh" | tr " " "\n" | fzf`
+echo "Installing $SCRIPT"
+read -p "Where?[~/.local/bin/]: " binpath
+binpath=${binpath:-~/.local/bin/}
+cp $SCRIPT $binpath
